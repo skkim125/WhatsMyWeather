49c39c22786890882b4a0e374cb3f0083097e791
skkim125 <kthanks125@gmail.com>
2024-06-19 14:07:58 +0900

    Initial Commit

---
+++ WhatsMyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsMyWeather
+//
+//  Created by 김상규 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
